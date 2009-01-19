4df57184a36ccf094e817f66bc04c3b1f84fd816
Jussi Uusi-Illikainen <jui@amelia.local>
2009-01-19 18:12:38 +0200

    validates_email_with_smtp method

--- lib/validates_email_with_smtp.rb
+++ lib/validates_email_with_smtp.rb
@@ -8,8 +8,12 @@ module ValidatesEmailWithSmtp
   end
 
   module ClassMethods
-    def validates_email_with_smtp(*args)
-      
+    def validates_email_with_smtp(*attrs)
+      validates_each(attrs) do |record,attr,value|
+        if MailCheck.run(value).invalid?
+          record.errors.add(attr,:invalid, :value => value)
+        end
+      end
     end
   end
 end
