f27fc5d28aadd5a63462d147391afd864ee3fa54
Jussi Uusi-Illikainen <jui@amelia.local>
2009-01-19 16:20:23 +0200

    init.rb and validates_email_with_smtp module

---
+++ lib/validates_email_with_smtp.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/mail_check'
+
+module VaildatesEmailWithSmtp
+  def self.included(base)
+    base.class_eval do
+      extend ClassMethods
+    end
+  end
+
+  module ClassMethods
+    def validates_email_with_smtp(*args)
+      
+    end
+  end
+end
+
+class ActiveRecord::Base
+  include ValidatesEmailWithSmtp
+end
